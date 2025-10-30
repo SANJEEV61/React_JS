0291551d11a3a40d9402fa18cac53090a77b5fe9
SANJEEV61 <sanjeev06022001@gmail.com>
2025-10-30 16:48:54 +0530

    changes updated day2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
 import './styles.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-       
+    
        <div className="container">
         <Header></Header>   
        </div>
       
-      <footer className= "footer">
-        <p className='footer'>© 2025 Moviedux. All rights reserved.</p>
-      </footer>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/MoviesGrid.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react';
+import React from "react";
+import '../styles.css';
+
+export default function MoviesGrid() {
+    const [movies, setMovies] = useState([]);
+    
+ return(
+    <div></div>
+ );
+}
