e2e3bab141f3a12f279a63cec74d2bb657f3fa4c
SANJEEV61 <sanjeev06022001@gmail.com>
2025-10-31 15:37:07 +0530

    changes updated day3

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -13,11 +13,16 @@ useEffect(() => {
     }, []);
 
  return(
-    <div className='movie-grid'>
+    <div className='movies-grid'>
         {
-        movies.map((movie) => (
-            <div key = {movie.id} className='movie-card'>
-                <img src={`images/${movie.image}`} alt={movie.title} className='movie-poster'/>     
+        movies.map(movie => (
+            <div key = {movie.id} className='movie-card'>              
+                <img src={`images/${movie.image}`} alt={movie.title}/>
+                <div className='movie-card-info'>
+                    <h3 className='movie-card-title'>{movie.title}</h3>
+                    <p className='movie-card-genre'>{movie.genre}</p>
+                    <p className='movie-card-rating'>{movie.rating}</p>
+                </div>  
             </div>
          ))
         }  
