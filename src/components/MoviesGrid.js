8bb636e7784d4377d4fd779e434cbee8ad91457b
SANJEEV61 <sanjeev06022001@gmail.com>
2025-10-31 15:03:13 +0530

    changes updated day3

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -4,8 +4,23 @@ import '../styles.css';
 
 export default function MoviesGrid() {
     const [movies, setMovies] = useState([]);
-    
+
+useEffect(() => {
+        fetch("movies.json")
+        .then((response) => response.json())
+        .then((data) => setMovies(data));
+
+    }, []);
+
  return(
-    <div></div>
+    <div className='movie-grid'>
+        {
+        movies.map((movie) => (
+            <div key = {movie.id} className='movie-card'>
+                <img src={`images/${movie.image}`} alt={movie.title} className='movie-poster'/>     
+            </div>
+         ))
+        }  
+    </div>
  );
 }
