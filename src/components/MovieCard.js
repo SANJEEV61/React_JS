7761000efedc92f68821b694ac79e2724c3ed210
SANJEEV61 <sanjeev06022001@gmail.com>
2025-11-05 16:47:08 +0530

    changes updated day4

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,8 +1,22 @@
 import React from "react";
-import '../styles.css';
+import "../styles.css";
 
-export default function MovieCard() {
-    return (
-       <div></div>
-    );
+export default function MovieCard({ movie }) {
+  const handleError = (e) => {
+    e.target.src = "images/default.jpg";
+  };
+  return (
+    <div key={movie.id} className="movie-card">
+      <img
+        src={`images/${movie.image}`}
+        alt={movie.title}
+        onError={handleError}
+      />
+      <div className="movie-card-info">
+        <h3 className="movie-card-title">{movie.title}</h3>
+        <p className="movie-card-genre">{movie.genre}</p>
+        <p className="movie-card-rating">{movie.rating}</p>
+      </div>
+    </div>
+  );
 }
